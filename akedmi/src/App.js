93b0342e46be345eae16d55db83476ad91369a0a
farina.riaz <farinariaz573@gmail.com>
2025-05-09 17:40:11 +0500

    fix the dashboard navigation blink issue

--- akedmi/src/App.js
+++ akedmi/src/App.js
@@ -1,5 +1,13 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useContext } from "react";
+
+import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
 import Company from "./pages/Company";
 import ChartOfAccount from "./pages/ChartOfAccount";
@@ -7,20 +15,65 @@ import Employee from "./pages/Employee";
 import InventoryItem from "./pages/InventoryItem";
 import Payroll from "./pages/Payroll";
 import Project from "./pages/Project";
-import { CompanyProvider } from "./store/CompanyContext";
-import Sidebar from "./components/Sidebar";
 import Users from "./pages/Users/Users";
-import { UserProvider } from "./store/UserContext";
 import NewCompany from "./pages/Company/NewCompany";
-import { EmployeeProvider } from "./store/EmployeeContext";
-import { PartnerProvider } from "./store/PartnerContext";
 import AddEmployee from "./pages/Employee/AddEmployee";
 import Partner from "./pages/Partner/Partner";
 import PartnerForm from "./pages/Partner/partnerform";
 import UserForm from "./pages/Users/Userform";
 import SignupAndLogin from "./pages/SignupAndLogin";
-import { AuthProvider } from "./store/signupAndLoginContext";
 import Settings from "./pages/Settings";
+
+import { AuthProvider, AuthContext } from "./store/signupAndLoginContext";
+import { CompanyProvider } from "./store/CompanyContext";
+import { EmployeeProvider } from "./store/EmployeeContext";
+import { PartnerProvider } from "./store/PartnerContext";
+import { UserProvider } from "./store/UserContext";
+
+const AppContent = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return (
+    <Router>
+      <Routes>
+        {!isAuthenticated ? (
+          <Route path="*" element={<SignupAndLogin />} />
+        ) : (
+          <Route
+            path="/*"
+            element={
+              <div className="flex">
+                <Sidebar />
+                <div className="flex-1 bg-[#F3F4FF] w-full overflow-y-auto h-screen">
+                  <Routes>
+                    <Route
+                      path="/"
+                      element={<Navigate to="/dashboard" replace />}
+                    />
+                    <Route path="/dashboard" element={<Home />} />
+                    <Route path="/company" element={<Company />} />
+                    <Route path="/addCompany" element={<NewCompany />} />
+                    <Route path="/partner" element={<Partner />} />
+                    <Route path="/add-partner" element={<PartnerForm />} />
+                    <Route path="/payroll" element={<Payroll />} />
+                    <Route path="/project" element={<Project />} />
+                    <Route path="/employee" element={<Employee />} />
+                    <Route path="/addEmployee" element={<AddEmployee />} />
+                    <Route path="/users" element={<Users />} />
+                    <Route path="/add-user" element={<UserForm />} />
+                    <Route path="/chart" element={<ChartOfAccount />} />
+                    <Route path="/inventory" element={<InventoryItem />} />
+                    <Route path="/settings" element={<Settings />} />
+                  </Routes>
+                </div>
+              </div>
+            }
+          />
+        )}
+      </Routes>
+    </Router>
+  );
+};
 
 function App() {
   return (
@@ -29,49 +82,7 @@ function App() {
         <EmployeeProvider>
           <UserProvider>
             <CompanyProvider>
-              <Router>
-                <Routes>
-                  <Route path="/" element={<SignupAndLogin />} />
-                  <Route
-                    path="/*"
-                    element={
-                      <div className="flex">
-                        <Sidebar />
-                        <div className="flex-1 bg-[#F3F4FF] w-full overflow-y-auto h-screen">
-                          <Routes>
-                            <Route path="/dashboard" element={<Home />} />
-                            <Route path="/company" element={<Company />} />
-                            <Route path="/partner" element={<Partner />} />
-                            <Route
-                              path="/add-partner"
-                              element={<PartnerForm />}
-                            />
-                            <Route path="/payroll" element={<Payroll />} />
-                            <Route path="/project" element={<Project />} />
-                            <Route path="/employee" element={<Employee />} />
-                            <Route
-                              path="/addEmployee"
-                              element={<AddEmployee />}
-                            />
-                            <Route path="/users" element={<Users />} />
-                            <Route path="/add-user" element={<UserForm />} />
-                            <Route path="/chart" element={<ChartOfAccount />} />
-                            <Route
-                              path="/inventory"
-                              element={<InventoryItem />}
-                            />
-                            <Route
-                              path="/addCompany"
-                              element={<NewCompany />}
-                            />
-                            <Route path="/settings" element={<Settings />} />
-                          </Routes>
-                        </div>
-                      </div>
-                    }
-                  />
-                </Routes>
-              </Router>
+              <AppContent />
             </CompanyProvider>
           </UserProvider>
         </EmployeeProvider>
